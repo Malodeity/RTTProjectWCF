508b467cb90228107e6ae8fadca9512a45c70139
Malibongwe Mthethwa <32899926+Malodeity@users.noreply.github.com>
2017-10-26 10:50:52 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace RTTProjectWCF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtResAddress = new System.Windows.Forms.TextBox();
+            this.txtWoAddress = new System.Windows.Forms.TextBox();
+            this.txtPosAddress = new System.Windows.Forms.TextBox();
+            this.txtCellNum = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.dgvEmployees = new System.Windows.Forms.DataGridView();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtWorkNum = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(48, 88);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(225, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Surname:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Residential Address:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(45, 315);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(80, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Postal Address:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(45, 262);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Work Address:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(45, 367);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Cell Number:";
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(48, 140);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(225, 20);
+            this.txtSurname.TabIndex = 11;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(48, 179);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(225, 20);
+            this.txtEmail.TabIndex = 12;
+            this.txtEmail.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtResAddress
+            // 
+            this.txtResAddress.Location = new System.Drawing.Point(48, 227);
+            this.txtResAddress.Name = "txtResAddress";
+            this.txtResAddress.Size = new System.Drawing.Size(225, 20);
+            this.txtResAddress.TabIndex = 13;
+            // 
+            // txtWoAddress
+            // 
+            this.txtWoAddress.Location = new System.Drawing.Point(48, 278);
+            this.txtWoAddress.Name = "txtWoAddress";
+            this.txtWoAddress.Size = new System.Drawing.Size(225, 20);
+            this.txtWoAddress.TabIndex = 14;
+            // 
+            // txtPosAddress
+            // 
+            this.txtPosAddress.Location = new System.Drawing.Point(48, 331);
+            this.txtPosAddress.Name = "txtPosAddress";
+            this.txtPosAddress.Size = new System.Drawing.Size(225, 20);
+            this.txtPosAddress.TabIndex = 15;
+            // 
+            // txtCellNum
+            // 
+            this.txtCellNum.Location = new System.Drawing.Point(48, 383);
+            this.txtCellNum.Name = "txtCellNum";
+            this.txtCellNum.Size = new System.Drawing.Size(225, 20);
+            this.txtCellNum.TabIndex = 16;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnAdd.Location = new System.Drawing.Point(48, 482);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(225, 23);
+            this.btnAdd.TabIndex = 17;
+            this.btnAdd.Text = "Add Employee";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(489, 367);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(318, 23);
+            this.btnView.TabIndex = 18;
+            this.btnView.Text = "View";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // dgvEmployees
+            // 
+            this.dgvEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployees.Location = new System.Drawing.Point(297, 88);
+            this.dgvEmployees.Name = "dgvEmployees";
+            this.dgvEmployees.Size = new System.Drawing.Size(653, 263);
+            this.dgvEmployees.TabIndex = 19;
+            this.dgvEmployees.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmployees_CellContentClick);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(489, 424);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(318, 23);
+            this.btnExport.TabIndex = 20;
+            this.btnExport.Text = "Export Data";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Poor Richard", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(408, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 36);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "RTT Interface";
+            // 
+            // txtWorkNum
+            // 
+            this.txtWorkNum.Location = new System.Drawing.Point(48, 441);
+            this.txtWorkNum.Name = "txtWorkNum";
+            this.txtWorkNum.Size = new System.Drawing.Size(225, 20);
+            this.txtWorkNum.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 424);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Work Number:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(962, 556);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtWorkNum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.dgvEmployees);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtCellNum);
+            this.Controls.Add(this.txtPosAddress);
+            this.Controls.Add(this.txtWoAddress);
+            this.Controls.Add(this.txtResAddress);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Employee";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtResAddress;
+        private System.Windows.Forms.TextBox txtWoAddress;
+        private System.Windows.Forms.TextBox txtPosAddress;
+        private System.Windows.Forms.TextBox txtCellNum;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.DataGridView dgvEmployees;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtWorkNum;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RTTProjectWCF.ServiceReference1;
+using System.IO;
+
+namespace RTTProjectWCF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Employee p = new Employee();
+            p.surname = txtName.Text;
+            p.surname = txtSurname.Text;
+            p.email = txtEmail.Text;
+            p.resAddress = txtResAddress.Text;
+            p.workAddress = txtWoAddress.Text;
+            p.postAddress = txtPosAddress.Text;
+            p.cellNum = txtCellNum.Text;
+            p.workNum = txtWorkNum.Text;
+           
+
+            Service1Client service = new Service1Client();
+
+           if(service.InsertEmployee(p) == 1)
+            {
+                MessageBox.Show("Employee was added successfully");
+                txtName.Clear();
+                txtSurname.Clear();
+                txtEmail.Clear();
+                txtResAddress.Clear();
+                txtWoAddress.Clear();
+                txtPosAddress.Clear();
+                txtCellNum.Clear();
+                txtWorkNum.Clear();
+
+            }
+        }
+
+        private void dgvEmployees_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+
+
+        }
+
+        private void btnView_Click(object sender, EventArgs p)
+        {
+
+
+            //List<Employee> GetEmployee = new List<Employee>();
+            Service1Client service = new Service1Client();
+            dgvEmployees.DataSource = service.GetEmployee();
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            //======WRITING TO FILE===============
+            TextWriter writer = new StreamWriter(@"C:\Users\User\Desktop\Final\Text.txt");
+            for(int m = 0; m < dgvEmployees.Rows.Count; m++)
+            {
+                for(int j = 0; j < dgvEmployees.Columns.Count-2; j++)
+                {
+                    writer.Write(""+dgvEmployees.Rows[m].Cells[j].Value.ToString()+""+", ");
+
+                }
+           
+                writer.WriteLine("");
+                writer.WriteLine("-------------------------------RTT EMPLOYEES------------------------");
+                writer.WriteLine("");
+            }
+            writer.Close();
+            MessageBox.Show("Data was successfully exported");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Service1Client service = new Service1Client();
+            dgvEmployees.DataSource = service.GetEmployee();
+        }
+    }
+}
